f51812963e644cb3505d73a5b63a71d9ba2edf74
mudit <mudit.shukla_cs19@gla.ac.in>
2021-07-11 17:56:56 +0530

    completed if satellite is above a particular place project. Whenever the satellite is above us the program will mail us telling that the satellite is above

--- projects/splashcardgame/main.py
+++ projects/splashcardgame/main.py
@@ -4,7 +4,7 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 try:
-    data = pandas.read_csv("data/word_to_learn.csv").to_dict(orient = "records")
+    data = pandas.read_csv("data/word_to_learn.csv").to_dict(orient ="records")
 except FileNotFoundError:
     data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
 current_dict = {}
@@ -34,23 +34,23 @@ def is_known():
 window = Tk()
 window.title("Flashy")
 window.config(bg = BACKGROUND_COLOR, padx = 50, pady = 50)
-image2 = PhotoImage(file = "images/card_front.png")
+image2 = PhotoImage(file ="images/card_front.png")
 
 
 flip_timer = window.after(3000, filp_card)
 
 canvas = Canvas(width = 800, height = 526, highlightthickness = 0, bg = BACKGROUND_COLOR)
-image = PhotoImage(file = "images/card_back.png")
+image = PhotoImage(file ="images/card_back.png")
 image_background = canvas.create_image((400,263), image = image )
 french_title = canvas.create_text((400, 150), text = "Title", font = ("Arial", 40, "italic"))
 french_text = canvas.create_text((400, 265), text = "bye-bye", font = ("Arial", 35, "bold"))
 canvas.grid(row = 0,column = 0, columnspan = 2)
 
 
-right_button_image = PhotoImage(file = "images/right.png", )
+right_button_image = PhotoImage(file ="images/right.png", )
 right_button = Button(image = right_button_image, highlightthickness = 0, bg = BACKGROUND_COLOR,command = is_known)
 right_button.grid(row = 1,column = 0)
-wrong_button_image = PhotoImage(file = "images/wrong.png")
+wrong_button_image = PhotoImage(file ="images/wrong.png")
 wrong_button = Button(image = wrong_button_image, highlightthickness = 0, bg = BACKGROUND_COLOR, command = choose_word)
 wrong_button.grid(row = 1, column = 1)
 
